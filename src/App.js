196fdf2b5da61bda5b4404683d31d234cdccd884
GentilleauF <francois.gentilleau@gmail.com>
2023-12-14 16:23:19 +0100

    creation shoping list

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Banner from "./components/Banner";
+import Cart from "./components/Cart";
+import ShoppingList from "./components/ShopingList";
+import './styles/index.css';
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Cart />
+      <ShoppingList/>
+    </div>
+  );
+}
+
+export default App;
